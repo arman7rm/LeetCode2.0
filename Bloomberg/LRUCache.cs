04e4d0ed8935e5be26eadbd96c7009963fecaebe
arman7rm <arman97rm@gmail.com>
2024-11-27 17:21:38 -0500

    updated

--- Bloomberg/LRUCache.cs
+++ Bloomberg/LRUCache.cs
@@ -1,74 +1,86 @@
-public class LRUCache {
+public class LRUCache
+{
     public Dictionary<int, Node> map;
     public int cap;
     public int size;
     public Node head;
     public Node tail;
 
-    public LRUCache(int capacity) {
+    public LRUCache(int capacity)
+    {
         this.cap = capacity;
         this.map = new Dictionary<int, Node>();
     }
-    
-    public int Get(int key) {
-        if(!map.ContainsKey(key)) return -1;
+
+    public int Get(int key)
+    {
+        if (!map.ContainsKey(key)) return -1;
         Node node = map[key];
-        if(node!=tail){
-            removeNode(node);
-            addToTail(node);
-        }
+        if (node != tail) moveToTail(node);
         return node.val;
     }
-   
-    public void Put(int key, int value) {
-        if(head==null){
+
+    public void Put(int key, int value)
+    {
+        if (head == null)
+        {
             head = new Node(value, key);
             size++;
             map[key] = head;
             tail = head;
-            return;
         }
-        if(map.ContainsKey(key)){
-            map[key].val = value;
-            if(map[key]==tail)return;
-            removeNode(map[key]);
-            addToTail(map[key]);
+        else if (map.ContainsKey(key))
+        {
+            Node node = map[key];
+            node.val = value;
+            if (node != tail) moveToTail(node);
         }
-        else{
+        else
+        {
             size++;
             addToTail(new Node(value, key));
             map[key] = tail;
-            if(size>cap){
+            if (size > cap)
+            {
                 map.Remove(head.key);
-                removeNode(head);
+                head = head.next;
+                head.prev = null;
             }
         }
     }
 
-    private void removeNode(Node n){
-        if(n==head){
+    private void moveToTail(Node n)
+    {
+        if (n == head)
+        {
             head = head.next;
             head.prev = null;
-        }else{
-            n.prev.next = n.next;
-            if (n.next != null) n.next.prev = n.prev;
-            else tail = n.prev; // Update tail if necessary
         }
+        else
+        {
+            n.prev.next = n.next;
+            n.next.prev = n.prev;
+        }
+        addToTail(n);
     }
-    private void addToTail(Node n){
+
+    private void addToTail(Node n)
+    {
         tail.next = n;
         n.prev = tail;
         n.next = null;
         tail = tail.next;
     }
 }
-public class Node {
+public class Node
+{
     public int val;
     public int key;
     public Node next;
     public Node prev;
 
-    public Node(int x, int key){
+    public Node(int x, int key)
+    {
         this.val = x;
         this.key = key;
     }
