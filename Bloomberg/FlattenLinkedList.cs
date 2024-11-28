55c25e3cb60b16769c5c1362914b9589e2f794e2
arman7rm <arman97rm@gmail.com>
2024-11-27 20:03:38 -0500

    flatten linked list

--- Bloomberg/FlattenLinkedList.cs
+++ Bloomberg/FlattenLinkedList.cs
@@ -8,34 +8,21 @@ public partial class Solution {
         }
     }
     public Node Flatten(Node head) {
-        if (head == null) return null;
-
-        var stack = new Stack<Node>();
         var runner = head;
-
-        while (runner != null) {
-            if (runner.child != null) {
-                // If there's a next node, push it onto the stack
-                if (runner.next != null) {
-                    stack.Push(runner.next);
-                }
-
-                // Flatten the child list
+        var stack = new Stack<Node>();
+        while(runner!=null){
+            if(runner.child!=null){
+                if (runner.next != null)stack.Push(runner.next);
                 runner.next = runner.child;
                 runner.next.prev = runner;
-                runner.child = null; // Clear the child pointer
+                runner.child = null;
             }
-
-            // If we reach the end of the current list and there are nodes on the stack
-            if (runner.next == null && stack.Count > 0) {
-                var nextNode = stack.Pop();
-                runner.next = nextNode;
-                nextNode.prev = runner;
+            if(runner.next==null && stack.Count>0){
+                runner.next = stack.Pop();
+                runner.next.prev = runner;
             }
-
             runner = runner.next;
         }
-
         return head;
     }
 }
